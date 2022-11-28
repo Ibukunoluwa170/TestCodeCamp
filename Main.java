fcf1a1f0310d7fd40bb86e96b7d77a461688d3b2
Ibukunoluwa170 <ibukun.dada2002@gmail.com>
2022-11-28 21:11:18 +0100

    First Java Project T14

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first Java Project");
+        System.out.println("I learnt about the PROs and CONs of Manual and Automation testing");
+        System.out.println("I can create Repository on git and also use the git command to push a project");
+        System.out.println("working on API testing has been fun and i've been able to learn the following:");
+        System.out.println("creating an environment,extracting Data from Api response,");
+        System.out.println("Running collection,Monitor Collections,Run newman Test and lastly use Selenium tool to test web applications");
+        System.out.println("Like i introduced above,this is my first Java coding and I am excited and can't wait to be a professional in using Java ");
+    }
+
+}
